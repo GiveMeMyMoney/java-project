04226f280102b0e627bb06b3cd2512c069fade7a
Marcin Styczen <test@github.com>
2015-04-08 19:13:41 +0200

    Dodalem projekt z Eclipse o nazwie PIZZERIA

---
+++ pizzeria/src/pizzeria/Main.java
@@ -0,0 +1,14 @@
+package pizzeria;
+
+public class Main {
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nie wiem co robie :D");
+		System.out.println("Czy GIT dzia³a?");
+	}
+
+}
